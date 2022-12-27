f102e03263620c1caed91b16689b39dc3ab5e4b6
Lucas Dias Torres <lucas.torres@luizalabs.com>
2022-12-27 15:29:03 -0300

    chore: router and partial home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import { ThemeProvider } from 'styled-components';
 import originTheme from './styles/theme/origin';
 
 import Header from './components/Header';
+import Home from './pages/Home';
 import BuyHousePage from './pages/BuyHouse/BuyHousePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'buy-a-house',
+    element: <BuyHousePage />,
+  },
+]);
 
 export const App = (): JSX.Element => (
   <ThemeProvider theme={originTheme}>
     <Header />
-    <BuyHousePage />
+    <RouterProvider router={router} />
   </ThemeProvider>
 );
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react';
+import Title from '../../components/Title';
+
+export const Home = (): ReactElement => {
+  return (
+    <>
+      <Title>
+        <>
+          Let&apos;s plan your <strong>saving goal.</strong>
+        </>
+      </Title>
+    </>
+  );
+};
+
+export default Home;
